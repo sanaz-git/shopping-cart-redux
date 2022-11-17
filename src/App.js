e4908b551cad79432f05da3787a64628457c8984
Sanaz <szeraati69@gmail.com>
2022-11-17 12:20:09 +0330

    edit something

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 // Components
 import Store from './components/Store';
@@ -7,23 +8,20 @@ import ProductDetails from './components/ProductDetails';
 import Navbar from './components/shared/Navbar';
 import ShopCart from './components/ShopCart';
 
-// Context
-import ProductContextProvider from './context/ProductContextProvider';
-import CartContextProvider from './context/CartContextProvider';
+//Redux
+import store from './redux/store';
 
 function App() {
   return (
-    <ProductContextProvider>
-      <CartContextProvider>
-        <Navbar />
-        <Switch>
-          <Route path="/products/:id" component={ProductDetails} />
-          <Route path="/products" component={Store} />
-          <Route path="/cart" component={ShopCart} />
-          <Redirect to="/products" />
-        </Switch>
-      </CartContextProvider>
-    </ProductContextProvider>
+    <Provider store={store}>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/products" component={Store} />
+        {/* <Route path="/cart" component={ShopCart} /> */}
+        <Redirect to="/products" />
+      </Switch>
+    </Provider>
   );
 }
 
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,28 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 import Product from './shared/Product';
+import Loader from './shared/Loader';
 
 // Context
-import { ProductsContext } from '../context/ProductContextProvider';
+// import { ProductsContext } from '../context/ProductContextProvider';
+
+//redux
+import { fetchProducts } from '../redux/products/productsAction';
 
 // Style
-import styles from "./Store.module.css";
+import styles from './Store.module.css';
 
 const Store = () => {
+  // const products = useContext(ProductsContext);
+  const dispatch = useDispatch();
+  const productsState = useSelector((state) => state.productsState);
 
-    const products = useContext(ProductsContext)
+  useEffect(() => {
+    if (!productsState.products.length) dispatch(fetchProducts());
+  }, []);
 
-    return (
-        <div className={styles.container} >
-            {
-                products.map(product => <Product 
-                        key={product.id} 
-                        productData = {product}
-                    />)
-            }
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      {
+        /* {products.map((product) => (
+        <Product key={product.id} productData={product} />
+      ))} */
+        productsState.loading ? (
+          <Loader />
+        ) : productsState.error ? (
+          <p>Something went wrong</p>
+        ) : (
+          productsState.products.map((product) => (
+            <Product key={product.id} productData={product} />
+          ))
+        )
+      }
+    </div>
+  );
 };
 
 export default Store;
